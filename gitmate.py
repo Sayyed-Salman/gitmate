9de3e16a54c40a91d3a7b7b9df5ec3f6511fea23
Sayyed-Salman <sayyedsalman2000@gmail.com>
2022-07-16 00:22:20 +0530

    📥 first commit

---
+++ gitmate.py
@@ -0,0 +1,151 @@
+import os
+import argparse
+import json
+import namegenerator
+
+parser = argparse.ArgumentParser()
+
+# general arguments
+parser.add_argument("--name", help="Repository name")
+parser.add_argument("-d", "--description",
+                    help="Description about the repository")
+parser.add_argument("-p", "--path", help="Local path")
+
+
+# User Data arguments
+parser.add_argument("--set-username", help="sets github username")
+parser.add_argument("--set-auth-token",
+                    help="set your github personal access token")
+
+
+args = parser.parse_args()
+
+
+def check_value(argument):
+    if argument:
+        return True
+
+
+def set_data(key, value):
+    with open("user_data.json", "+r") as file:
+        user_data = json.load(file)
+        user_data[key] = value
+        file.seek(0)
+        json.dump(user_data, file, indent=4)
+        file.truncate()
+
+
+USER_DATA = None
+with open("user_data.json", "r") as file:
+    USER_DATA = json.load(file)
+
+if USER_DATA['auth_token'] == "":
+    print("* Git Mate *")
+    print("set your personal access token first")
+    print("gitmate --set-auth-token \"your_personal_access_token\"")
+    exit
+
+
+PROJECT_NAME = args.name if check_value(args.name) else namegenerator.gen()
+PROJECT_DESCRIPTION = args.description if check_value(
+    args.description) else "This is a new repository"
+PATH = args.path if check_value(args.path) else "./"
+
+
+def create_local_repo(name, path):
+    """
+    Creates local repository and returns project path
+    """
+    if not os.path.isdir(path):
+        project_path = os.path.join(path, name)
+        os.chdir(path)
+        os.mkdir(project_path)
+    return project_path
+
+
+def git_init(path):
+    """
+    Initializes github repository, returns system response.
+    """
+    if os.path.isdir(path):
+        os.chdir(path)
+        system_response = os.system("git init")
+    return system_response
+
+
+def add_readme(path, title, description):
+    """
+        Create a README.md file in project dir root.
+    """
+    if os.path.isdir(path):
+        os.chdir(path)
+        file_content = f""" # {title}
+        {description}
+        """
+        file_path = os.path.join(path, "README.md")
+        with open("README.md", 'w+') as file:
+            file.write(file_content)
+
+    if os.path.isfile(file_path):
+        return 0
+
+
+# testing add_readme() func
+"""
+result = add_readme('A:\\Test\\monkey', "Monkey-Code",
+                    "Writing Monkey Code to analyze how monkey brain works")
+print(result)
+test passed 
+"""
+
+
+def stage_git(path, commands: list):
+    """
+    Stating changes to git and then commiting
+    returns system response, check while debugging
+    """
+    if os.path.isdir(path):
+        os.chdir(path)
+        sys_response = []
+        for cmd in commands:
+            try:
+                response = os.system(cmd)
+                sys_response.append(response)
+            except Exception as e:
+                return (e, sys_response)
+        return 0
+
+
+# testing stage_git()
+"""
+result = stage_git('A:\\Test\\monkey', [
+                   "git add .", "git commit -m \"first commit\""])
+print(result)
+test passed
+"""
+
+
+def adding_remote_repository(path, remote_link):
+    if os.path.isdir(path):
+        os.chdir(path)
+        sys_log = []
+        try:
+            add_remote = os.system(f"git remote add origin {remote_link}")
+            branch = os.system("git branch -M main")
+            sys_log.append(add_remote)
+            sys_log.append(branch)
+        except Exception as e:
+            return (e, sys_log)
+
+        return 0
+
+
+def git_push(path):
+    if os.path.isdir(path):
+        os.chdir(path)
+        sys_log = []
+        try:
+            push = os.system("git push -u origin main")
+            sys_log.append(push)
+        except Exception as e:
+            return (e, sys_log)
---
+++ rest_client.py
@@ -0,0 +1,70 @@
+import requests
+# import namegenerator
+
+
+# sample code for rest api
+"""
+auth_token = 'your_personal_access_token'
+
+hed = {'Authorization': 'token ' + auth_token}
+data = {'name': 'test-repo1', 'description': 'New Repository'}
+
+url = "https://api.github.com/user/repos"
+
+response = requests.post(url, json=data, headers=hed)
+print(response.json())
+
+"""
+
+
+class RestClient:
+    """
+    REST client for github request 
+    """
+
+    def __init__(self, auth_token, data, url) -> None:
+        self.auth_token = auth_token
+        self.data = data
+        self.url = url
+        self.head = {"Authorization": f"token {self.auth_token}"}
+        self.response = None
+
+    def create_post_request(self) -> requests.Response:
+        """
+        Calls GitHub Api and create a response object.
+        """
+        self.response = requests.post(
+            url=self.url, headers=self.head, json=self.data)
+        return self.response
+
+    def pretty_response(self):
+        """
+        Decorating Response for output
+        """
+        data = self.response.json()
+        name = data["name"]
+        url_path = data["full_name"]
+        url = "https://www.github.com/"+url_path
+        self.git_url = url + ".git"
+        print(
+            f"STATUS => {self.response}\nREPOSITORY NAME => {name}\nGITHUB URL => {url}")
+        return self.git_url
+
+
+# Testing RestClient() class
+"""
+# Test Data
+
+newname = namegenerator.gen()
+token = "your_personal_access_token"
+data = {"name": f"{newname}", "description": "This is a repository."}
+url = "https://api.github.com/user/repos"
+
+
+github = RestClient(token, data, url)
+response = github.create_post_request()
+github.pretty_response()
+print(github.git_url)
+
+test passed
+"""
