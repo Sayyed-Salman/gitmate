f1e818acfc0f4fa8da3c1f84e616be6e870ae5ab
Sayyed-Salman <sayyedsalman2000@gmail.com>
2022-07-22 01:23:11 +0530

    ✔️ First build

--- gitmate.py
+++ gitmate.py
@@ -1,7 +1,9 @@
 import os
 import argparse
 import json
+from urllib import response
 import namegenerator
+from rest_client import RestClient
 
 
 def check_value(argument):
@@ -18,7 +20,6 @@ def set_data(key, value):
         file.truncate()
 
 
-# TODO : check if user is using --set-auth flag and save it in user_data.json file
 # user_data.json format
 """
 {
@@ -27,9 +28,6 @@ def set_data(key, value):
   "projectsFolder":""
 }
 """
-# Run in __name__
-
-# run in __name__
 
 
 def create_local_repo(name, path):
@@ -136,7 +134,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # general arguments
-    parser.add_argument("--name", help="Repository name")
+    parser.add_argument("-n", "--name", help="Repository name")
     parser.add_argument("-d", "--description",
                         help="Description about the repository")
     parser.add_argument("-p", "--path", help="Local path")
@@ -144,27 +142,61 @@ if __name__ == "__main__":
     parser.add_argument("--set-username", help="sets github username")
     parser.add_argument("--set-auth-token",
                         help="set your github personal access token")
+    parser.add_argument("--set-projects-folder",
+                        help="set a default project folder")
     args = parser.parse_args()
 
-    PROJECT_NAME = args.name if check_value(args.name) else namegenerator.gen()
+    PROJECT_NAME = args.name if check_value(
+        args.name) else namegenerator.gen()
     PROJECT_DESCRIPTION = args.description if check_value(
         args.description) else "This is a new repository"
-    PATH = args.path if check_value(args.path) else "./"
     USER_DATA = None
+    URL = "https://api.github.com/user/repos"
+    AUTH_TOKEN = None
+    PATH = args.path if check_value(args.path) else "./"
 
     if args.set_auth_token:
         token = args.set_auth_token
         set_data("auth_token", token)
-        # with open("./user_data.json","+r") as file :
-        #     user_data = json.load()
-        #     user_data["auth_token"] = token
+
+    elif args.set_projects_folder:
+        folder_loc = args.set_projects_folder
+        set_folder = False
+        while not set_folder:
+            if os.path.isdir:
+                set_data("projectsFolder", folder_loc)
+                set_folder = True
+            else:
+                print("[!] Invalid Folder Location ")
+                folder_loc = input(
+                    "Enter Project Folder Location again (absolute path): ")
 
     else:
         with open("user_data.json", "r") as file:
             USER_DATA = json.load(file)
+            AUTH_TOKEN = USER_DATA["auth_token"]
+            PROJECT_FOLDER = USER_DATA["projectsFolder"]
 
         if USER_DATA['auth_token'] == "":
             print("* Git Mate *")
             print("set your personal-access-token")
             print("gitmate --set-auth-token \"your_personal_access_token\"")
             exit
+        elif USER_DATA["projectsFolder"] == "":
+            print("[!] Set your default projects folder ")
+            print("[cmd] gitmate --set-projects-folder")
+
+    if USER_DATA["projectsFolder"] != "":
+        PATH = USER_DATA["projectsFolder"]
+
+    create_local_repo(PROJECT_NAME, PATH)
+    git_init(PATH)
+    add_readme(PATH, PROJECT_NAME, PROJECT_DESCRIPTION)
+    git_commands = ["git add .", "git commit -m \"Initial commit 🗳️\""]
+    stage_git(PATH, git_commands)
+    data = {"name": f"{PROJECT_NAME}", "description": f"{PROJECT_DESCRIPTION}"}
+    github = RestClient(AUTH_TOKEN, data, URL)
+    github.create_post_request()
+    REMOTE_URL = github.pretty_response()
+    adding_remote_repository(PATH, REMOTE_URL)
+    git_push(PATH)
